084cc23af177dcf825a166eda826a16a4cb0f26b
Tbbas <alldentobias@gmail.com>
2018-10-19 21:42:08 +0200

    Added index and signup. Imported router

---
+++ src/components/Index.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello, World!</h1>
+            <p>We like beer!</p>
+            </div>
+        )
+    }
+}
+export default Index;
