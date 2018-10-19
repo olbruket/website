6e9a6a5b2c1fac63188e40a35dabbb2472ebb6c3
Tbbas <alldentobias@gmail.com>
2018-10-19 21:57:02 +0200

    Added materialUO

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,12 +1,16 @@
 import React,{Component} from 'react';
+import Paper from '@material-ui/core/Paper';
+
 
 
 class Index extends Component {
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: '#6202EE', color: 'white', width: '100vw', height: '100vh'}}>
+            <div style={{paddingTop: '20em', paddingLeft: '40vw'}}>
             <h1>Hello, World!</h1>
             <p>We like beer!</p>
+            </div>
             </div>
         )
     }
