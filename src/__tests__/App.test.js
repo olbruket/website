d62de6f3bc8a9a1509b683f5edae41d47738dd38
Johannes Mattsson <karlmattsson96@gmail.com>
2018-11-05 21:37:17 +0100

    More better linting rules, now indentation works

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,9 +6,9 @@ import App from '../App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  , div);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/MemberSignup.js
+++ src/components/MemberSignup.js
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
 
 class MemberSignup extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Signup</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Signup</h1>
+      </div>
+    )
+  }
 }
 export default MemberSignup;
