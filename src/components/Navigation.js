043559efd2396ec113c0b1d0d5e298eab12d5a08
Johannes Mattsson <karlmattsson96@gmail.com>
2018-11-05 22:07:27 +0100

    Added navigation to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
+import { NavLink } from 'react-router-dom';
 
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+
+import logo from '../logo.svg';
 
 const Navigation = ({ children }) => (
   <div>
     <AppBar>
       <Toolbar>
-        <Typography>
-          Menues are cool
-        </Typography>
+        <Button
+          link
+          to={'/'}
+          component={NavLink}
+        >
+          <img src={logo} style={ {maxWidth: '7em' }}/>
+        </Button>
+        <Button
+          link
+          to={'/signup'}
+          component={NavLink}
+        >
+          Become a member!
+        </Button>
       </Toolbar>
     </AppBar>
     <div style={ {paddingTop: '5em' }}>
