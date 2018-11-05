040ec3680ff8a20bb4702a2840f1741f7d74613f
Johannes Mattsson <karlmattsson96@gmail.com>
2018-11-05 21:37:04 +0100

    Renamed index component to homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Index from './components/Index';
+import Home from './components/Home';
 import MemberSignup from './components/MemberSignup';
 
 import './App.css';
@@ -8,12 +8,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <div>
-            <Switch>
-                <Route exact path ="/" component={ Index } />
-                <Route path="/signup" component={ MemberSignup } />
-            </Switch>
-        </div>
+      <div>
+        <Switch>
+          <Route exact path ="/" component={ Home } />
+          <Route path="/signup" component={ MemberSignup } />
+        </Switch>
+      </div>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import logo from '../logo.svg';
+
+const Home = () => {
+  <div className={'message'} style={{width: '100vw', height: '70vh'}}>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '70vh' }}
+    >
+      <Grid item xs={6}>
+        <div>
+          <img src={logo} />
+          <Typography component="h4" variant="display4" color={'default'}>
+                                Hello there!
+          </Typography>
+        </div>
+      </Grid>
+      <Grid item xs={6}>
+        <p>This page is under active development, please check back later!</p>
+      </Grid>
+
+    </Grid>
+  </div>
+}
+export default Home;
